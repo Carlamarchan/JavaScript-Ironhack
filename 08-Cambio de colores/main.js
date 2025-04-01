66b2db986348c0772c8b64be5d5000f0198d7b9e
Carla Marchán <carlamarchan93@gmail.com>
2025-04-01 21:40:21 +0200

    Cambio de colores

---
+++ 08-Cambio de colores/main.js
@@ -0,0 +1,22 @@
+const elemento1 = document.querySelector("#boton1");
+const caja = document.querySelector("#caja");
+const texto = document.querySelector("h3");
+const circulo = document.querySelector(".circulo");
+const mensaje = document.getElementById("message").classList;
+
+function rojo() {
+  caja.style.backgroundColor = "red";
+}
+
+function verde() {
+  texto.style.color = "green";
+}
+
+function azul() {
+  circulo.style.backgroundColor = "blue";
+}
+
+function descubre() {
+  mensaje.add("color");
+  mensaje.add("size");
+}
